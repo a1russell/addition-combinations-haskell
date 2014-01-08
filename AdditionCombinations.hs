d2c9a318ac2d985d588850d6119eef3dda9f72f8
Adam Russell <adamlr6+pub@gmail.com>
2014-01-07 21:01:43 -0600

    Re-indent.

--- AdditionCombinations.hs
+++ AdditionCombinations.hs
@@ -9,43 +9,50 @@ type Combos = [Combo]
 
 additionCombinations :: Int -> Set.Set Combo
 additionCombinations 0 = Set.empty
-additionCombinations x = let
+additionCombinations x =
+  let
     go allCombos@(headCombo:_)
-        | all (==1) headCombo = allCombos
-        | otherwise = go $ newCombos ++ allCombos
-        where newCombo = expandedTailCombination headCombo
-              newCombos = newCombo : collapsedTailCombos newCombo
-    in Set.fromList $ go [[x]]
+      | all (==1) headCombo = allCombos
+      | otherwise = go $ newCombos ++ allCombos
+      where
+        newCombo = expandedTailCombination headCombo
+        newCombos = newCombo : collapsedTailCombos newCombo
+  in
+    Set.fromList $ go [[x]]
 
 expandedTailCombination :: Combo -> Combo
 expandedTailCombination (x:xs) = (x - 1 : 1 : xs)
 
 collapsedTailCombos :: Combo -> Combos
-collapsedTailCombos = let
+collapsedTailCombos =
+  let
     go _ [] = []
     go splitIndex combo@(x:xs)
-        | length combo < splitIndex = [combo]
-        | otherwise = collapsed ++ combosForHead ++ combosForSplit
-        where collapsed = Maybe.catMaybes [headCollapsed, splitCollapsed]
-              headCollapsed = collapseInto 1 combo
-              splitCollapsed = collapseInto splitIndex combo
-              combosForHead = emptyOr collapsedTailCombos headCollapsed
-              combosForSplit = emptyOr combosForNextSplit splitCollapsed
-              combosForNextSplit = go $ splitIndex + 1
-              emptyOr = maybe []
-    in go 2
+      | length combo < splitIndex = [combo]
+      | otherwise = collapsed ++ combosForHead ++ combosForSplit
+      where
+        collapsed = Maybe.catMaybes [headCollapsed, splitCollapsed]
+        headCollapsed = collapseInto 1 combo
+        splitCollapsed = collapseInto splitIndex combo
+        combosForHead = emptyOr collapsedTailCombos headCollapsed
+        combosForSplit = emptyOr combosForNextSplit splitCollapsed
+        combosForNextSplit = go $ splitIndex + 1
+        emptyOr = maybe []
+  in
+    go 2
 
 collapseInto :: Int -> Combo -> Maybe Combo
 collapseInto _ [] = Nothing
 collapseInto splitIndex xs
-    | splitIndex < 1 = Nothing
-    | length rhs < numberToCollapse = Nothing
-    | collapsedRhsHead > last lhs = Nothing
-    | otherwise = Just $ lhs ++ collapsedRhsHead : newRhsTail
-    where splitXs = splitAt splitIndex xs
-          lhs = fst splitXs
-          rhs = snd splitXs
-          numberToCollapse = 2
-          splitRhs = splitAt numberToCollapse rhs
-          collapsedRhsHead = sum $ fst splitRhs
-          newRhsTail = snd splitRhs
+  | splitIndex < 1 = Nothing
+  | length rhs < numberToCollapse = Nothing
+  | collapsedRhsHead > last lhs = Nothing
+  | otherwise = Just $ lhs ++ collapsedRhsHead : newRhsTail
+  where
+    splitXs = splitAt splitIndex xs
+    lhs = fst splitXs
+    rhs = snd splitXs
+    numberToCollapse = 2
+    splitRhs = splitAt numberToCollapse rhs
+    collapsedRhsHead = sum $ fst splitRhs
+    newRhsTail = snd splitRhs
--- AdditionCombinationsTest.hs
+++ AdditionCombinationsTest.hs
@@ -6,69 +6,70 @@ import Test.HUnit
 import AdditionCombinations
 
 additionCombinationsTests =
-    [ "finds no combinations for 0" ~: do
-        Set.fromList [] @=? additionCombinations 0
-    , "finds combination for 2" ~: do
-        Set.fromList [[1, 1], [2]] @=? additionCombinations 2
-    , "finds combinations for 7" ~: do
-        let combinationsOf7 =
-                [ [1, 1, 1, 1, 1, 1, 1]
-                , [2, 2, 2, 1]
-                , [2, 2, 1, 1, 1]
-                , [2, 1, 1, 1, 1, 1]
-                , [3, 2, 2]
-                , [3, 3, 1]
-                , [3, 2, 1, 1]
-                , [3, 1, 1, 1, 1]
-                , [4, 3]
-                , [4, 2, 1]
-                , [4, 1, 1, 1]
-                , [5, 2]
-                , [5, 1, 1]
-                , [6, 1]
-                , [7]
-                ]
-        Set.fromList combinationsOf7 @=? additionCombinations 7
-    ]
+  [ "finds no combinations for 0" ~: do
+    Set.fromList [] @=? additionCombinations 0
+  , "finds combination for 2" ~: do
+    Set.fromList [[1, 1], [2]] @=? additionCombinations 2
+  , "finds combinations for 7" ~: do
+    let
+      combinationsOf7 =
+        [ [1, 1, 1, 1, 1, 1, 1]
+        , [2, 2, 2, 1]
+        , [2, 2, 1, 1, 1]
+        , [2, 1, 1, 1, 1, 1]
+        , [3, 2, 2]
+        , [3, 3, 1]
+        , [3, 2, 1, 1]
+        , [3, 1, 1, 1, 1]
+        , [4, 3]
+        , [4, 2, 1]
+        , [4, 1, 1, 1]
+        , [5, 2]
+        , [5, 1, 1]
+        , [6, 1]
+        , [7]
+        ]
+    Set.fromList combinationsOf7 @=? additionCombinations 7
+  ]
 
 expandedTailCombinationTests =
-    [ "separates 1 from 2" ~: do
-        [1, 1] @=? expandedTailCombination [2]
-    , "moves 1 from 3 into tail of [3, 1]" ~: do
-        [2, 1, 1] @=? expandedTailCombination [3, 1]
-    ]
+  [ "separates 1 from 2" ~: do
+    [1, 1] @=? expandedTailCombination [2]
+  , "moves 1 from 3 into tail of [3, 1]" ~: do
+    [2, 1, 1] @=? expandedTailCombination [3, 1]
+  ]
 
 collapsedTailCombosTests =
-    [ "finds 3 tails when given [3, 1, 1, 1, 1]" ~: do
-        3 @=? (length . collapsedTailCombos $ [3, 1, 1, 1, 1])
-    , "finds [3, 2, 1, 1] when finding tails of [3, 1, 1, 1, 1]" ~: do
-        True @=? (elem [3, 2, 1, 1] $ collapsedTailCombos [3, 1, 1, 1, 1])
-    , "finds [3, 2, 2] when finding tails of [3, 1, 1, 1, 1]" ~: do
-        True @=? (elem [3, 2, 2] $ collapsedTailCombos [3, 1, 1, 1, 1])
-    , "finds [3, 3, 1] when finding tails of [3, 1, 1, 1, 1]" ~: do
-        True @=? (elem [3, 3, 1] $ collapsedTailCombos [3, 1, 1, 1, 1])
-    ]
+  [ "finds 3 tails when given [3, 1, 1, 1, 1]" ~: do
+    3 @=? (length . collapsedTailCombos $ [3, 1, 1, 1, 1])
+  , "finds [3, 2, 1, 1] when finding tails of [3, 1, 1, 1, 1]" ~: do
+    True @=? (elem [3, 2, 1, 1] $ collapsedTailCombos [3, 1, 1, 1, 1])
+  , "finds [3, 2, 2] when finding tails of [3, 1, 1, 1, 1]" ~: do
+    True @=? (elem [3, 2, 2] $ collapsedTailCombos [3, 1, 1, 1, 1])
+  , "finds [3, 3, 1] when finding tails of [3, 1, 1, 1, 1]" ~: do
+    True @=? (elem [3, 3, 1] $ collapsedTailCombos [3, 1, 1, 1, 1])
+  ]
 
 collapseIntoTests =
-    [ "does not collapse when split at less than 1" ~: do
-        Nothing @=? collapseInto 0 [1]
-    , "does not collapse when right-hand side of split is empty" ~: do
-        Nothing @=? collapseInto 1 [1]
-    , "does not collapse when right-hand side of split has only one element" ~:
-        do Nothing @=? collapseInto 1 [2, 1]
-    , "does not collapse when rhs head >= tail of one-element lhs list" ~: do
-        Nothing @=? collapseInto 1 [2, 2, 1]
-    , "does not collapse when rhs sum > tail of one-element lhs list" ~: do
-        Nothing @=? collapseInto 1 [3, 2, 2]
-    , "does not collapse when rhs head >= tail of two-element lhs list" ~: do
-        Nothing @=? collapseInto 2 [3, 2, 2, 1]
-    , "collapses 1 into head of two-element right-hand side list" ~: do
-        (Just [3, 3]) @=? collapseInto 1 [3, 2, 1]
-    , "collapses 1 into rhs head when both sides have 2 elements" ~: do
-        (Just [4, 3, 3]) @=? collapseInto 2 [4, 3, 2, 1]
-    , "collapses 1 into head of three-element rhs list" ~: do
-        (Just [3, 3, 1]) @=? collapseInto 1 [3, 2, 1, 1]
-    ]
+  [ "does not collapse when split at less than 1" ~: do
+    Nothing @=? collapseInto 0 [1]
+  , "does not collapse when right-hand side of split is empty" ~: do
+    Nothing @=? collapseInto 1 [1]
+  , "does not collapse when right-hand side of split has only one element" ~:
+    do Nothing @=? collapseInto 1 [2, 1]
+  , "does not collapse when rhs head >= tail of one-element lhs list" ~: do
+    Nothing @=? collapseInto 1 [2, 2, 1]
+  , "does not collapse when rhs sum > tail of one-element lhs list" ~: do
+    Nothing @=? collapseInto 1 [3, 2, 2]
+  , "does not collapse when rhs head >= tail of two-element lhs list" ~: do
+    Nothing @=? collapseInto 2 [3, 2, 2, 1]
+  , "collapses 1 into head of two-element right-hand side list" ~: do
+    (Just [3, 3]) @=? collapseInto 1 [3, 2, 1]
+  , "collapses 1 into rhs head when both sides have 2 elements" ~: do
+    (Just [4, 3, 3]) @=? collapseInto 2 [4, 3, 2, 1]
+  , "collapses 1 into head of three-element rhs list" ~: do
+    (Just [3, 3, 1]) @=? collapseInto 1 [3, 2, 1, 1]
+  ]
 
 tests = test $ expandedTailCombinationTests
              ++ collapsedTailCombosTests
