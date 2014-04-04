9c8680ac3eca17f6b82fbd098b7cec4b632cd2a3
Adam Russell <adamlr6+pub@gmail.com>
2014-04-03 22:19:42 -0400

    Accept command line argument.

--- src/main/haskell/Main.hs
+++ src/main/haskell/Main.hs
@@ -1,6 +1,25 @@
+import qualified Control.Monad.Trans.Maybe as MaybeT
+import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
+import qualified System.Environment as Env
+import qualified System.Exit as Exit
+import qualified System.IO as IO
+import qualified Text.Read as Read
 
 import AdditionCombinations
 
 main :: IO ()
-main = mapM_ print $ Set.toList . additionCombinations $ 5
+main = do
+  args <- Env.getArgs
+  arg <- MaybeT.runMaybeT $
+    let
+      liftMaybe = MaybeT.MaybeT . return
+    in do
+      firstArg <- liftMaybe . Maybe.listToMaybe $ take 1 args
+      number <- liftMaybe . Read.readMaybe $ firstArg
+      return number
+  case arg of
+    (Just arg') -> mapM_ print $ Set.toList . additionCombinations $ arg'
+    Nothing -> do
+      IO.hPutStrLn IO.stderr $ "Please provide a valid number to partition."
+      Exit.exitFailure
