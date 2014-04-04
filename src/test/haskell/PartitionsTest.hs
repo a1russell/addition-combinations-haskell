6c8c7b8818acda7d90fb8efec5fd875e97e112fe
Adam Russell <adamlr6+pub@gmail.com>
2014-04-04 14:02:27 -0400

    Clean up test.

--- src/test/haskell/PartitionsTest.hs
+++ src/test/haskell/PartitionsTest.hs
@@ -3,18 +3,18 @@ module PartitionsTest
   ) where
 
 import qualified Data.Set as Set
-import qualified Data.Maybe as Maybe
 
 import Test.HUnit
 
 import Partitions
 
+partitionsTests :: [Test]
 partitionsTests =
-  [ "finds no partitions for 0" ~: do
+  [ "finds no partitions for 0" ~:
     Set.fromList [] @=? partitions 0
-  , "finds partitions for 2" ~: do
+  , "finds partition for 2" ~:
     Set.fromList [[1, 1], [2]] @=? partitions 2
-  , "finds partitions for 7" ~: do
+  , "finds partitions for 7" ~:
     let
       partitionsOf7 =
         [ [1, 1, 1, 1, 1, 1, 1]
@@ -33,59 +33,65 @@ partitionsTests =
         , [6, 1]
         , [7]
         ]
-    Set.fromList partitionsOf7 @=? partitions 7
+    in
+      Set.fromList partitionsOf7 @=? partitions 7
   ]
 
+expandedTailPartitionTests :: [Test]
 expandedTailPartitionTests =
-  [ "separates 1 from 2" ~: do
+  [ "separates 1 from 2" ~:
     [1, 1] @=? expandedTailPartition [2]
-  , "moves 1 from 3 into tail of [3, 1]" ~: do
+  , "moves 1 from 3 into tail of [3, 1]" ~:
     [2, 1, 1] @=? expandedTailPartition [3, 1]
   ]
 
+collapsedTailPartitionsTests :: [Test]
 collapsedTailPartitionsTests =
-  [ "finds 3 tails when given [3, 1, 1, 1, 1]" ~: do
+  [ "finds 3 tails when given [3, 1, 1, 1, 1]" ~:
     3 @=? (length . collapsedTailPartitions $ [3, 1, 1, 1, 1])
-  , "finds [3, 2, 1, 1] when finding tails of [3, 1, 1, 1, 1]" ~: do
+  , "finds [3, 2, 1, 1] when finding tails of [3, 1, 1, 1, 1]" ~:
     True @=? (elem [3, 2, 1, 1] $ collapsedTailPartitions [3, 1, 1, 1, 1])
-  , "finds [3, 2, 2] when finding tails of [3, 1, 1, 1, 1]" ~: do
+  , "finds [3, 2, 2] when finding tails of [3, 1, 1, 1, 1]" ~:
     True @=? (elem [3, 2, 2] $ collapsedTailPartitions [3, 1, 1, 1, 1])
-  , "finds [3, 3, 1] when finding tails of [3, 1, 1, 1, 1]" ~: do
+  , "finds [3, 3, 1] when finding tails of [3, 1, 1, 1, 1]" ~:
     True @=? (elem [3, 3, 1] $ collapsedTailPartitions [3, 1, 1, 1, 1])
   ]
 
+collapseIntoTests :: [Test]
 collapseIntoTests =
-  [ "does not collapse when split at less than 1" ~: do
+  [ "does not collapse when split at less than 1" ~:
     Nothing @=? collapseInto 0 [1]
-  , "does not collapse when right-hand side of split is empty" ~: do
+  , "does not collapse when right-hand side of split is empty" ~:
     Nothing @=? collapseInto 1 [1]
   , "does not collapse when right-hand side of split has only one element" ~:
-    do Nothing @=? collapseInto 1 [2, 1]
-  , "does not collapse when rhs head >= tail of one-element lhs list" ~: do
+    Nothing @=? collapseInto 1 [2, 1]
+  , "does not collapse when rhs head >= tail of one-element lhs list" ~:
     Nothing @=? collapseInto 1 [2, 2, 1]
-  , "does not collapse when rhs sum > tail of one-element lhs list" ~: do
+  , "does not collapse when rhs sum > tail of one-element lhs list" ~:
     Nothing @=? collapseInto 1 [3, 2, 2]
-  , "does not collapse when rhs head >= tail of two-element lhs list" ~: do
+  , "does not collapse when rhs head >= tail of two-element lhs list" ~:
     Nothing @=? collapseInto 2 [3, 2, 2, 1]
-  , "collapses 1 into head of two-element right-hand side list" ~: do
+  , "collapses 1 into head of two-element right-hand side list" ~:
     (Just [3, 3]) @=? collapseInto 1 [3, 2, 1]
-  , "collapses 1 into rhs head when both sides have 2 elements" ~: do
+  , "collapses 1 into rhs head when both sides have 2 elements" ~:
     (Just [4, 3, 3]) @=? collapseInto 2 [4, 3, 2, 1]
-  , "collapses 1 into head of three-element rhs list" ~: do
+  , "collapses 1 into head of three-element rhs list" ~:
     (Just [3, 3, 1]) @=? collapseInto 1 [3, 2, 1, 1]
   ]
 
+partitionsCountTests :: [Test]
 partitionsCountTests =
-  [ "count of partitions of -1 is 0" ~: do
+  [ "count of partitions of -1 is 0" ~:
     0 @=? partitionsCount (-1)
-  , "count of partitions of 0 is 1" ~: do
+  , "count of partitions of 0 is 1" ~:
     1 @=? partitionsCount 0
-  , "count of partitions of 1 is 1" ~: do
+  , "count of partitions of 1 is 1" ~:
     1 @=? partitionsCount 1
-  , "count of partitions of 10 is 42" ~: do
+  , "count of partitions of 10 is 42" ~:
     42 @=? partitionsCount 10
   ]
 
+tests :: [Test]
 tests = expandedTailPartitionTests
      ++ collapsedTailPartitionsTests
      ++ collapseIntoTests
