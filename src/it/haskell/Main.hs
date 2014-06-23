3233963d6efd0c51c1e10540f4aeaf03a5be0042
Adam Russell <adamlr6+pub@gmail.com>
2014-06-23 02:37:32 -0400

    Add QuickCheck test suite.

---
+++ src/it/haskell/Main.hs
@@ -0,0 +1,23 @@
+import Control.Monad (liftM)
+import qualified Data.Set as Set
+import qualified System.Exit
+import Test.QuickCheck
+import Test.QuickCheck.Test (isSuccess)
+
+import Partitions
+
+prop_partitionSumIsArgument :: Int -> Bool
+prop_partitionSumIsArgument x =
+  all (\y -> sum y == x) (Set.toList . partitions $ x)
+
+main :: IO ()
+main =
+  let
+    isSuccessM = liftM isSuccess
+    checkTwentyTimes = quickCheckWithResult stdArgs { maxSuccess = 20 }
+    checkLimitedRange = checkTwentyTimes . forAll (choose (0, 42))
+  in do
+    testsPassed <- isSuccessM . checkLimitedRange $ prop_partitionSumIsArgument
+    if testsPassed
+      then System.Exit.exitSuccess
+      else System.Exit.exitFailure
